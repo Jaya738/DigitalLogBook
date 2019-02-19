9f500b0f31e78380a54d74265bc7063bfefdc5d5
lakshman mallidi <lakshman.mallidi@gmail.com>
2019-02-20 00:42:04 +0530

    Added create database option.

--- managerooms.py
+++ managerooms.py
@@ -1,47 +1,49 @@
-import MySQLdb
+import mysql.connector
 import sys
-import getpass
 try:
-    if len(sys.argv) == 2:
-        credits_user = sys.argv[1]
-        credits_psw = getpass.getpass("Enter password:")
-        conn = MySQLdb.connect(host="localhost",
-                               user=credits_user,
-                               passwd=credits_psw,
-                               db="your_database_name")
-        c = conn.cursor()
-        while True:
-            choice = int(raw_input("-------------------------\nklef log book\n-------------------------\n1.Create New Room\n2.Delete Room\n3.Show all Room\n4.Exit\nplease choose an option:"))
-            if choice > 0 and choice < 5:
-                if choice == 1:
-                    room = raw_input("Enter the Room number:")
-                    c.execute("SELECT * FROM rooms where room='{}'".format(room))
-                    if len(c.fetchall()) < 1:
-                        c.execute("INSERT INTO rooms(room) VALUES('{}')".format(room))
-                        c.execute("CREATE TABLE {}(si INT NOT NULL PRIMARY KEY AUTO_INCREMENT,userId INT NOT NULL,purpose VARCHAR(30),equipmentid VARCHAR(20),datecol date NOT NULL,intime time NOT NULL,outtime time)".format(room))
-                    else:
-                        print "The Room number is already existed"
-                elif choice == 2:
-                    room = raw_input("Enter the Room number you want to delete:")
-                    c.execute("SELECT room FROM rooms where room='{}'".format(room))
-                    if len(c.fetchall()) > 0:
-                        c.execute("DELETE FROM rooms where room='{}'".format(room))
-                        c.execute("DROP TABLE {}".format(room))
-                    else:
-                        print "Room number not existed"
-                elif choice == 3:
-                    c.execute("SELECT room FROM rooms")
-                    print "----------------------\nRooms\n----------------------\n"
-                    for row in c.fetchall():
-                        print row[0]
+    ipaddress = raw_input("Enter your database server host address:")
+    user_db = raw_input("Enter username:")
+    password_db = raw_input("Enter password:")
+    db_name = raw_input("Enter database name:")
+    conn = mysql.connector.connect(host=ipaddress,
+                                   user=user_db,
+                                   passwd=password_db,
+                                   db=db_name)
+    c = conn.cursor()
+    while True:
+        choice = int(raw_input("-------------------------\nklef log book\n-------------------------\n1.Create New Room\n2.Delete Room\n3.Show all Room\n4.Initialize database\n5.Exit\nplease choose an option:"))
+        if choice > 0 and choice < 6:
+            if choice == 1:
+                room = raw_input("Enter the Room number:")
+                password = raw_input("Enter the password:")
+                c.execute("SELECT * FROM rooms where room='{}'".format(room))
+                if len(c.fetchall()) < 1:
+                    c.execute("INSERT INTO rooms(room,password) VALUES('{}','{}')".format(room,password))
+                    c.execute("CREATE TABLE {}(si INT NOT NULL PRIMARY KEY AUTO_INCREMENT,userId INT NOT NULL,purpose VARCHAR(30),equipmentid VARCHAR(20),datecol date NOT NULL,intime time NOT NULL,outtime time)".format(room))
                 else:
-                    break
+                    print "The Room number is already existed"
+            elif choice == 2:
+                room = raw_input("Enter the Room number you want to delete:")
+                c.execute("SELECT room FROM rooms where room='{}'".format(room))
+                if len(c.fetchall()) > 0:
+                    c.execute("DELETE FROM rooms where room='{}'".format(room))
+                    c.execute("DROP TABLE {}".format(room))
+                else:
+                    print "Room number not existed"
+            elif choice == 3:
+                c.execute("SELECT room FROM rooms")
+                print "----------------------\nRooms\n----------------------\n"
+                for row in c.fetchall():
+                    print row[0]
+            elif choice == 4:
+                c.execute("CREATE TABLE rooms(room varchar(10) NOT NULL, password varchar(20) NOT NULL)")
+                c.execute("CREATE TABLE sessioncode(code varchar(32) NOT NULL)")
             else:
-                print "please choose a valid option"
-        conn.commit()
-        c.close()
-        conn.close()
-    else:
-        print "USAGE: python manageusers.py [sqlusername]"
+                break
+        else:
+            print "please choose a valid option"
+    conn.commit()
+    c.close()
+    conn.close()
 except Exception as e:
     print e
